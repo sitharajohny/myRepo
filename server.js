04727bf29a80275ac94463177e2fdccb89513ecb
sitharajohny <sitharajohny@gmail.com>
2022-10-01 14:07:01 -0400

    changes added again

--- server.js
+++ server.js
@@ -11,8 +11,25 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 
 //http://localhost:3000/
 app.get('/', function (req, res) {
-    res.send("<h1>Home Page</h1>")
+    res.send("<h1>Home Page welcome sithara</h1>")
 })
+
+//http://localhost:3000/profile
+app.post('/profile', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+  })
+  
+  //http://localhost:3000/admin
+  app.get('/admin', (req, res) => {
+    res.send('Admin Homepage')
+  })
+  
+  //http://localhost:3000/user/100
+  app.get("/user/:id", (req, res)=> {
+        res.send(`User ID: ${req.params.id}`);
+      }
+  )
 
 app.listen(process.env.PORT  || SERVER_PORT, () => {
     console.log(`Server running at http://${SERVER_HOST}:${SERVER_PORT}/`);
